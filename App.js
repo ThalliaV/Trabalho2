16e80742ca45d2571de7811e4a832d3dba1153bb
ThalliaV <74866332+ThalliaV@users.noreply.github.com>
2025-11-17 19:27:18 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-//import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
 import Home from './src/Telas/Home';
 import Missoes from './src/Telas/Missoes';
 import Temporada from './src/Telas/Temporada';
+import Calendario from './src/Telas/Calendario';
+import ArtesComunidade from './src/Telas/ArtesComunidade';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +17,8 @@ export default function App() {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Missoes" component={Missoes} />
         <Stack.Screen name="Temporada" component={Temporada} />
+        <Stack.Screen name="Calendario" component={Calendario} />
+        <Stack.Screen name="ArtesComunidade" component={ArtesComunidade} />
       </Stack.Navigator>
     </NavigationContainer>
   );
